bdaead70b08208e303fd0f6e19286505147b1538
RaccoonNW <87783832+RaccoonNW@users.noreply.github.com>
2022-08-25 14:30:46 -0600

    added animation to home page text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,43 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 export default function Home() {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const nameArray = [' ', 'G', 'a', 'b', 'r', 'i', 'e', 'l']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+    useEffect(() => {
+        setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000)
+      }, [])
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1> Hi, <br /> I'm
-                    Gabriel 
-                    <br />
-                    web developer
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br />
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={15}
+
+                />
+                <br />
+                <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={22}
+
+                />
                 </h1>
                 <h2> JavaScript / Ruby </h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
